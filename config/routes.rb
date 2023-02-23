579ecfc48494e7f32ec5cf9d33de2995e274c716
Cristian Toffalo <cristian.toffalo@web-experto.com.ar>
2023-02-22 22:28:02 -0300

    Implement devise for session management

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  devise_for :users
   get 'home/index'
-  root "home#index"
+  root 'home#index'
+  #devise_scope :user do
+  #  root to: "devise/sessions#new"
+  #end 
 end
